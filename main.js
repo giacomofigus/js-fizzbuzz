60e29bc43d302a90b6ed53485bca8351f5295b91
MyUsername <figusgiacomo04@gmail.com>
2024-01-09 18:20:57 +0100

    feat (completo): for + bonus

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-
+/*
 const lista = document.querySelector("ul.myList")
 
-for(let i = 0; i <= 100; i++){
+for(let i = 1; i <= 100; i++){
     if(i % 3 === 0 && i % 5 === 0){
-        const element = `<li> FizzBuzz ${i} </li>`
+        const element = `<li style="color:purple;"> FizzBuzz ${i} </li>`
         lista.innerHTML += element
-        element.style.color = "purple"
 
     } else if(i % 3 === 0 ) {
         const element = `<li style="color: red;"> Fizz ${i} </li>`
@@ -22,3 +21,27 @@ for(let i = 0; i <= 100; i++){
     
     
 }
+*/
+const container = document.querySelector("myContainer");
+const lista = document.querySelector("ul.myList")
+
+for(let i = 1; i <= 100; i++ ){
+
+
+    if(i % 3 === 0 && i % 5 === 0){
+        const element = '<li class="box-fizzbuzz col-1"> FizzBuzz  </li>';
+        lista.innerHTML += element;
+    } else if (i % 3 === 0){
+        const element = '<li class ="box-fizz col-1"> Fizz </li>';
+        lista.innerHTML += element;
+    } else if (i % 5 === 0){
+        const element = '<li class= "box-buzz col-1"> Buzz </li>';
+        lista.innerHTML += element;
+    } else{
+        const element = `<li class="box-num col-1"> ${i} </li>`;
+        lista.innerHTML += element;
+    }
+        
+    
+}
+
