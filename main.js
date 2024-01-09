0894ed2b3df4e915c0b8478055a562885a9229bb
MyUsername <figusgiacomo04@gmail.com>
2024-01-09 16:34:19 +0100

    fix: style-color-js

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
-/*
-Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-Prima di partire a scrivere codice poniamoci qualche domanda:
-Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare che possiamo usare?
-Consigli del giorno:
-Scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-BONUS 1: Crea un container nel DOM , aggiungendo (attraverso la funzione append()) un elemento html con il numero o la stringa corretta da mostrare.
-BONUS 2: Applica stili differenti agli elementi aggiunti al DOM nel BONUS 1, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz. Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.
-*/
+
 const lista = document.querySelector("ul.myList")
 
 for(let i = 0; i <= 100; i++){
     if(i % 3 === 0 && i % 5 === 0){
-        const element = `<li style="color:purple;"> FizzBuzz ${i} </li>`
+        const element = `<li> FizzBuzz ${i} </li>`
         lista.innerHTML += element
+        element.style.color = "purple"
 
     } else if(i % 3 === 0 ) {
         const element = `<li style="color: red;"> Fizz ${i} </li>`
@@ -22,7 +14,7 @@ for(let i = 0; i <= 100; i++){
     } else if ((i % 5) === 0){
         const element = `<li style="color: blue;"> Buzz ${i} </li>`
         lista.innerHTML += element
-        
+         
     } else{
         const element = `<li style="color: black;"> ${i} </li>`
         lista.innerHTML += element
